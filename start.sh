3eac9015a77d2b59e865eac40648c3d982b8f270
Dante Barba <dantebarba@Dantes-iMac.local>
2020-09-24 18:59:35 -0300

    start qbittorrent

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 docker-compose up -d gdrive-move;
 docker-compose up -d gdrive-mount;
 sleep 5;
-docker-compose up -d deluge;
+docker-compose up -d qbittorrent;
 #ocker-compose up -d autoremove-torrents;
