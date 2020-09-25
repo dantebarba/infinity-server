96360e0809dd4d11f3f48bc54de44a134f8cc972
Dante Barba <dantebarba@Dantes-iMac.local>
2020-09-25 13:45:26 -0300

    autoremove-torrents re-enabled

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ docker-compose up -d gdrive-mount;
 sleep 5;
 docker-compose up -d qbittorrent;
 docker-compose up -d tracker-annunce;
-#ocker-compose up -d autoremove-torrents;
+docker-compose up -d autoremove-torrents;
