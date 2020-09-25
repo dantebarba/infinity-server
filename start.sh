59c85abef24eb13d08c6d2dc3e4e963d0d30f38a
Dante Barba <dantebarba@Dantes-iMac.local>
2020-09-24 23:22:19 -0300

    tracker annunce

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ docker-compose up -d gdrive-move;
 docker-compose up -d gdrive-mount;
 sleep 5;
 docker-compose up -d qbittorrent;
+docker-compose up -d tracker-annunce;
 #ocker-compose up -d autoremove-torrents;
