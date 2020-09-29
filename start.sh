ac0bf3139e918db64893ddb39bca7edfd319236b
Dante Barba <dantebarba@Dantes-iMac.local>
2020-09-28 23:08:57 -0300

    nzbget start

--- start.sh
+++ start.sh
@@ -3,5 +3,6 @@ docker-compose up -d gdrive-move;
 docker-compose up -d gdrive-mount;
 sleep 5;
 docker-compose up -d qbittorrent;
+docker-compose up -d nzbget;
 docker-compose up -d tracker-annunce;
 docker-compose up -d autoremove-torrents;
